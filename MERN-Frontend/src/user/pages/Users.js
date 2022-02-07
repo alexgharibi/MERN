e907f628804c7659ecb64c0adeaeebcd2c523500
alexgharibi <ali52346@gmail.com>
2022-02-07 17:45:35 -0500

    Add card and avatar component

--- MERN-Frontend/src/user/pages/Users.js
+++ MERN-Frontend/src/user/pages/Users.js
@@ -2,7 +2,16 @@ import React from "react";
 import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <UsersList />;
+  const USERS = [
+    {
+      id: "1",
+      name: "alex",
+      image:
+        "https://i.picsum.photos/id/1001/5616/3744.jpg?hmac=38lkvX7tHXmlNbI0HzZbtkJ6_wpWyqvkX4Ty6vYElZE",
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
