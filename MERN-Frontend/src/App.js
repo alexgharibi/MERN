95ee82318babbd87e7adaffa0ad6858519a4aad8
alexgharibi <ali52346@gmail.com>
2022-02-09 16:21:00 -0500

    add dynamic routing

--- MERN-Frontend/src/App.js
+++ MERN-Frontend/src/App.js
@@ -6,6 +6,7 @@ import {
   Switch,
 } from "react-router-dom";
 import Users from "./user/pages/Users";
+import UserPlaces from "./places/pages/UserPlaces";
 import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
@@ -17,6 +18,7 @@ const App = () => {
         <Switch>
           <Route path="/" exact component={Users} />
           <Route path="/places/new" exact component={NewPlace} />
+          <Route path="/:userId/places" exact component={UserPlaces} />
           <Redirect to="/" />
         </Switch>
       </main>
--- MERN-Frontend/src/user/pages/Users.js
+++ MERN-Frontend/src/user/pages/Users.js
@@ -4,7 +4,7 @@ import UsersList from "../components/UsersList";
 const Users = () => {
   const USERS = [
     {
-      id: "1",
+      id: "u1",
       name: "alex",
       image:
         "https://i.picsum.photos/id/1001/5616/3744.jpg?hmac=38lkvX7tHXmlNbI0HzZbtkJ6_wpWyqvkX4Ty6vYElZE",
